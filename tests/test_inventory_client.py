f4ea2f5164633d0ca9fc6e8b3af58f5ba61e18a8
Manel Montilla <manel.montilla@gmail.com>
2021-09-22 17:18:04 +0200

    tests: wrap lines to 79 chars

--- tests/test_inventory_client.py
+++ tests/test_inventory_client.py
@@ -213,17 +213,26 @@ def test_assets_pagination(cli, init_assets):
 def test_assets_type(cli, init_assets):
     """Tests the filter ``type`` of the method ``assets`` of the class
     ``InventoryClient``."""
+    expected = [
+        asset for asset in init_assets if asset.asset_id.type == 'type0'
+    ]
     assert compare_unsorted_list(
-        cli.assets(type='type0'), [asset for asset in init_assets if asset.asset_id.type == 'type0'], lambda x: x.vid)
+        cli.assets(type='type0'), expected, lambda x: x.vid)
 
 
-def test_assets_type(cli, init_assets):
+def test_assets_type_pagination(cli, init_assets):
     """Tests the filter ``type`` and the ``pagination`` params of the method
     ``assets`` of the class ``InventoryClient``."""
-    expected = [asset for asset in init_assets if asset.asset_id.type == 'type0']
+    expected = [
+        asset for asset in init_assets if asset.asset_id.type == 'type0'
+    ]
     expected = expected[:1]
     assert compare_unsorted_list(
-        cli.assets(page_idx=0, page_size=1, type='type0'), expected, lambda x: x.vid)
+        cli.assets(
+            page_idx=0, page_size=1, type='type0'
+        ),
+        expected, lambda x: x.vid
+    )
 
 
 def test_asset(cli, init_assets):
